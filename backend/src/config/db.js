ae6aff5345a4a26844d593ac051c476ef65ba0f0
nirvair09 <chandradeep262003@gmail.com>
2026-02-01 10:34:31 +0530

    feat: Initialize backend with Express server, basic route, and MongoDB connection.

---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("DB connected successfully");
+
+    } catch (error) {
+        console.log("DB connection failed");
+        process.exit(1);
+    }
+};
+
+export default connectDB;
