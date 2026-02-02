ecd33d540a2bafd0b7358a89e8ee45eb4836c395
nirvair09 <chandradeep262003@gmail.com>
2026-02-02 13:05:47 +0530

    feat: Implement initial user authentication with login and registration pages and set up application routing.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState } from "react";
+import api from "../api/axios";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const res = await api.post("/auth/login", { email, password });
+        login(res.data.token);
+        navigate("/dashboard");
+    };
+
     return (
-        <div>Login</div>
-    )
-}
+        <form onSubmit={submitHandler}>
+            <h2>Login</h2>
 
-export default Login
+            <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+            <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+
+            <button type="submit">Login</button>
+        </form>
+    );
+}
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-
+import { useState } from "react";
+import api from "../api/axios";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 const Register = () => {
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        password: "",
+        role: "customer",
+    });
+
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+
+        const res = await api.post("/auth/register", form);
+        login(res.data.token);
+        navigate("/dashboard");
+    }
     return (
-        <div>Register</div>
-    )
+        <form onSubmit={handleFormSubmit}>
+            <h2>Register Form</h2>
+            <input placeholder="name" value={form.name} onChange={e => setForm({ ...form, name: e.target.value })} />
+
+            <input placeholder="email" value={form.email} onChange={e => setForm({ ...form, email: e.target.value })} />
+
+            <input placeholder="password" value={form.password} onChange={e => setForm({ ...form, password: e.target.value })} />
+
+            <button type="submit" >Register</button>
+
+        </form>
+    );
+
 }
 
-export default Register
+export default Register;
