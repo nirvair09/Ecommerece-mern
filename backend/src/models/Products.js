948eef13e8205c2495f366ff48edd2f2718afd1c
nirvair09 <chandradeep262003@gmail.com>
2026-02-03 11:53:30 +0530

    feat: create Product Mongoose model schema with name, description, price, quantity, and seller fields.

---
+++ backend/src/models/Products.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    seller: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
+export default mongoose.model("Product", productSchema);
