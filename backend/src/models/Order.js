d88bb762c56f1742d8ad197a5510e46972d9cc71
nirvair09 <chandradeep262003@gmail.com>
2026-02-04 14:05:56 +0530

    feat: Add Mongoose Order and OrderItem models with associated schemas.

---
+++ backend/src/models/Order.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    name: string,
+    price: Number,
+    quantity: Number,
+    seller: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+},
+    { _id: false });
+
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+
+    },
+    items: [orderItemSchema],
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["pending", "shipped", "delivered", "cancelled"],
+        default: "pending"
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+export default mongoose.model("Order", orderSchema);
