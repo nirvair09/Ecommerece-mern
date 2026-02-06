61de766991d970923a57994b5a97c751623b1f5e
nirvair09 <chandradeep262003@gmail.com>
2026-02-06 09:04:31 +0530

    feat: add order routes for placing and retrieving orders, and integrate them into the main application.

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/products", productRoutes);
 app.use("/cart", cartRoutes);
+app.use("/orders", orderRoutes);
 
 app.get("/", (req, res) => {
     res.send("API Running...");
