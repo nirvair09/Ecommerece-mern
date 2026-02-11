6a5fb1ffac45c05ab6b76a1041675c0b1826bb3d
nirvair09 <chandradeep262003@gmail.com>
2026-02-11 13:29:48 +0530

    feat: Add admin login and endpoints for user and product management.

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -1,4 +1,7 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import Product from "../models/Product.js";
+import jwt from "jsonwebtoken";
 
 // get all users
 export const getAllUsers = async (req, res) => {
@@ -26,4 +29,27 @@ export const toggleProductApproval = async (req, res) => {
     product.isApproved = !product.isApproved;
     await product.save();
     res.json({ success: true, message: "Product approval toggled" });
+}
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    });
+};
+
+
+export const loginAdmin = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || user.role !== "admin") {
+        return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    const isMatch = bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.status(401).json({ success: false, message: "Invalid credentials" });
+    }
+    res.json(
+        { token: generateToken(user._id) }
+    );
 }
--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -2,10 +2,10 @@ import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { adminOnly } from "../middleware/adminMiddleware.js";
 import { getAllOrders } from "../controllers/orderController.js";
-import { toggleProductApproval, toggleUserActiveStatus } from "../controllers/adminController.js";
+import { loginAdmin, toggleProductApproval, toggleUserActiveStatus } from "../controllers/adminController.js";
 
 const router = express.Router();
-
+router.post("/login", loginAdmin);
 router.get("/orders", protect, adminOnly, getAllOrders);
 router.post("/toggle-user-active-status", protect, adminOnly, toggleUserActiveStatus);
 router.post("/toggle-product-approval", protect, adminOnly, toggleProductApproval);
